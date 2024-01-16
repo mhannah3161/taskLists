f881e8bfe6778ad0b8a760127d95ae28f6553a7c
Michael Hannah <Michaelrkhannah@gmail.com>
2024-01-16 16:00:07 -0700

    connected MONGO!

--- server/server.js
+++ server/server.js
@@ -4,23 +4,25 @@ const app = express();
 
 const cors = require("cors");
 
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use(require("./routes/record"));
 
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+//connect to database
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() =>
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        }
+        )
+    )
+.catch ((err) => console.log(err));
+
+
