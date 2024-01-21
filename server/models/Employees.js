31a9d8f6fc2088af0d503c0f7c07a62a8f1b9415
Michael Hannah <Michaelrkhannah@gmail.com>
2024-01-21 08:28:09 -0700

    Corrected Employees.js to have a model group and export everything, not just the employee sections.

--- server/models/Employees.js
+++ server/models/Employees.js
@@ -5,20 +5,43 @@ const employeeSchema = new mongoose.Schema({
       last_name: String,
       email: String,
       phone: String,
+      birthday: String,
+      hire_date: String,
       employee_notes: [employeeNotesSchema],
       collections: [collectionSchema],
   });
 
-  const employeeNotesSechema = new mongoose.Schema({
+  const employeeNotesSchema = new mongoose.Schema({
     employeeNotes_name: String,
     employeeNotes_description: String,
     });
+
     const collectionSchema = new mongoose.Schema({
         collection_id: mongoose.Schema.Types.ObjectId,
         collection_name: String,
         collection_correctiveActions: [correctiveActionSchema],collection_positives: [positiveSchema],
       });
 
-  const Employee= mongoose.model('Employee', employeeSchema);
+      const positiveSchema = new mongoose.Schema({
+        positive_name: String,
+        positive_pictures: String,
+        positive_description: String,
+        });
 
-  module.exports = Employee;
+        const correctiveActionSchema = new mongoose.Schema({
+            correctiveAction_name: String,
+            correctiveAction_pictures: String,
+            correctiveAction_description: String,
+            });
+        
+
+
+  const Employee= mongoose.model('Employee', employeeSchema);
+  const EmployeeNotes = mongoose.model('EmployeeNotes', employeeNotesSchema);
+    const Collection = mongoose.model('Collection', collectionSchema);
+    const CorrectiveAction = mongoose.model('CorrectiveAction', correctiveActionSchema);
+    const Positive = mongoose.model('Positive', positiveSchema);
+
+  module.exports = {
+    Employee, EmployeeNotes, Collection, CorrectiveAction, Positive
+  }
